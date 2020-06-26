61382851007871f4ddc341b4f961b2e4f4a12a45
sasha <sasha_sho@walla.com>
2020-06-26 19:33:46 +0300

    Added skeleton

---
+++ GitHubRepositoriesExplorer/MockDataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubRepositoriesExplorer
+{
+    public class MockDataService
+    {
+        private List<string> _tokens;
+
+        public MockDataService()
+        {
+            _tokens = new List<string>() { "a", "b" };
+        }
+
+        public List<string> GetTokens() => _tokens;
+    }
+}
